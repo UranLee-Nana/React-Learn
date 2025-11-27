67851b489234e9cff267d8212242a7d2d612deb2
UranLee-Nana <uranlxy235@gmail.com>
2025-11-27 23:39:57 +0800

    Day2 关于文档中的你的第一个组件

---
+++ react_day2/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Gallery from './Gallery.jsx'
+
+function App() {
+
+  return (
+    <section>
+      <h1>Amazing Scientist</h1>
+      <Gallery />
+
+    </section>
+  )
+}
+
+export default App
+
---
+++ react_day2/src/Gallery.jsx
@@ -0,0 +1,7 @@
+export default function Gallery() {
+  return (
+    <img
+      src="https://i.imgur.com/MK3eW3As.jpg"
+      alt="Katherine Johnson" />
+  )
+}
